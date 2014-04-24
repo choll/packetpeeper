3a364ac8ec03dbfa8865ea258dc71a65ad2c2c7a
Chris Holloway <c.holloway@gmail.com>
2014-04-25 00:29:45 +0100

    fix issue #17

--- make_dmg.sh
+++ make_dmg.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 rm -rf build/Deployment
+DATE=$(date '+%Y-%m-%d')
+sed s/BUNDLE_VERSION_PLACEHOLDER/$DATE/g PacketPeeper.plist.template > PacketPeeper.plist
 xcodebuild -target All -configuration Deployment
-hdiutil create build/Deployment/PacketPeeper_$(date '+%Y-%m-%d').dmg -srcfolder 'build/Deployment/Packet Peeper.app'
+hdiutil create build/Deployment/PacketPeeper_$DATE.dmg -volname "Packet Peeper" -srcfolder 'build/Deployment/Packet Peeper.app'
 
