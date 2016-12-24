c881ececac84081f0d93d4381f67147eaa475c89
choll <c.holloway@gmail.com>
2016-12-24 23:53:11 +0000

    Remove plist template, update make_dmg script not to use a template, update copyright year in plist

--- make_dmg.sh
+++ make_dmg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-rm -rf build/Release
 DATE=$(date '+%Y-%m-%d')
-sed s/BUNDLE_VERSION_PLACEHOLDER/$DATE/g PacketPeeper.plist.template > PacketPeeper.plist
+sed -i '.make_dmg_backup'  s/BUNDLE_VERSION_PLACEHOLDER/$DATE/g PacketPeeper.plist
 xcodebuild -target All -configuration Release
+mv PacketPeeper.plist.make_dmg_backup PacketPeeper.plist
 hdiutil create build/Release/PacketPeeper_$DATE.dmg -volname "Packet Peeper" -srcfolder 'build/Release/Packet Peeper.app'
 
