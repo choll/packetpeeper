610a916752e31b64a364898b68a90834a46ecdb1
choll <c.holloway@gmail.com>
2017-02-25 15:58:39 -0300

    Use bz2 for dmg compression, do shallow git clones for dependencies

--- fetch_and_build_dependencies.sh
+++ fetch_and_build_dependencies.sh
@@ -1,21 +1,23 @@
 #!/bin/sh -e
 
-target_dir=third_party_repos
+target_dir="third_party_repos"
+breakpad_branch="chrome_55"
+hexfiend_tag="v2.4.0"
 
 mkdir -p ${target_dir}
 cd ${target_dir}
 
-test -d breakpad || git clone --no-checkout https://github.com/google/breakpad.git
+test -d breakpad || git clone --depth 1 --branch ${breakpad_branch} --no-checkout https://github.com/google/breakpad.git
 cd breakpad
-git checkout chrome_55
+git checkout ${breakpad_branch}
 xcodebuild -project src/client/mac/Breakpad.xcodeproj -config Release
 xcodebuild -project src/tools/mac/dump_syms/dump_syms.xcodeproj -config Release
 cd ..
 test -L ../Breakpad.framework || ln -s ${target_dir}/breakpad/src/client/mac/build/Release/Breakpad.framework ../Breakpad.framework
 
-test -d HexFiend || git clone --no-checkout https://github.com/ridiculousfish/HexFiend.git
+test -d HexFiend || git clone --depth 1 --branch ${hexfiend_tag} --no-checkout https://github.com/ridiculousfish/HexFiend.git
 cd HexFiend
-git checkout v2.4.0
+git checkout ${hexfiend_tag}
 xcodebuild -target HexFiend_Framework -config Release
 cd ..
 test -L ../HexFiend.framework || ln -s ${target_dir}/HexFiend/build/Release/HexFiend.framework ../HexFiend.framework
--- make_dmg.sh
+++ make_dmg.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/sh -e
 ./fetch_and_build_dependencies.sh
 DATE=$(date '+%Y-%m-%d')
 sed -i '.make_dmg_backup'  s/BUNDLE_VERSION_PLACEHOLDER/$DATE/g PacketPeeper.plist
 xcodebuild -target All -configuration Release
 mv PacketPeeper.plist.make_dmg_backup PacketPeeper.plist
-hdiutil create build/Release/PacketPeeper_$DATE.dmg -volname "Packet Peeper" -srcfolder 'build/Release/Packet Peeper.app'
+hdiutil create -format UDBZ build/Release/PacketPeeper_$DATE.dmg -volname "Packet Peeper" -srcfolder 'build/Release/Packet Peeper.app'
 
