01903e9755f80865b034cfc81d9a43095ec4e340
choll <c.holloway@gmail.com>
2017-02-27 22:59:59 -0300

    Update port/services database, switch to using new fangled CSV file for importing service data

--- services2pp.py
+++ services2pp.py
@@ -18,11 +18,12 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import struct
+import sys
 
 datfile_tcp = "port-numbers-tcp.port"
 datfile_udp = "port-numbers-udp.port"
 porturl = "http://www.iana.org/assignments/port-numbers"
-portfile = "port-numbers"
+portfile = "service-names-port-numbers.csv"
 
 infile = open(portfile, 'r')
 datfile_tcp = open(datfile_tcp, 'w')
@@ -33,52 +34,43 @@ maxdesc = 0
 port2name = {}
 
 for line in infile.readlines():
-    # remove comments
-    line = line.split("#", 1)
+    # Service Name, Port Number, Transport Protocol, Description
+    toks = [x.strip() for x in line.split(",")]
 
-    if(line == [] or line[0] == ""):
+    if (len(toks) != 12):
         continue
 
-    # split on whitespace
-    tok = line[0].split(None, 2)
+    name = toks[0]
+    port = toks[1]
+    proto = toks[2]
+    description = toks[3]
 
-    if(len(tok) != 3):
+    if (proto != "tcp" and proto != "udp"):
         continue
 
-    tok2 = tok[1].split("/")
-
-    if(len(tok2) != 2):
+    if (not name or not port or not description):    
         continue
 
-    proto = tok2[1]
-
-    if(proto != "tcp" and proto != "udp"):
-        continue
-
-    name = tok[0]
-    description = tok[2].strip()
-
-    tok3 = tok2[0].split("-")
-
-    if(len(tok3) == 2):
-        lport = int(tok3[0])
-        hport = int(tok3[1])
+    toks = port.split("-")
+    if (len(toks) == 2):
+        lport = int(toks[0])
+        hport = int(toks[1])
     else:
-        lport = hport = int(tok2[0])
+        lport = hport = int(toks[0])
 
-    port = lport
-
-    if(len(name) > maxname):
+    if (len(name) > maxname):
         maxname = len(name)
 
-    if(len(description) > maxdesc):
+    if (len(description) > maxdesc):
         maxdesc = len(description)
 
-    if(port2name.has_key(port)):
-        port2name[port].append([name, description, proto])
-    else:
-        port2name[port] = [[name, description, proto]]
-
+    port = lport
+    while port <= hport:
+        if (port2name.has_key(port)):
+            port2name[port].append([name, description, proto])
+        else:
+            port2name[port] = [[name, description, proto]]
+        port += 1
 
 # file format;
 # 2 byte magic number.
@@ -111,32 +103,32 @@ while (port < pow(2,16)):
         # Skip over duplicate entries
         i = 0
         badService = False
-        while(i < (len(tcpservices) - 1)):
-            if(tcpservices[i][1] != tcpservices[i + 1][1]):
+        while (i < (len(tcpservices) - 1)):
+            if (tcpservices[i][1] != tcpservices[i + 1][1]):
                 print ("Service mismatch on port %u between:\n\t" % port) + tcpservices[i][1] + "\n\tand:\n\t" + tcpservices[i + 1][1] + "\n"
                 badService = True
             i = i + 1
 
-        if(badService or len(tcpservices) < 1):
+        if (badService or len(tcpservices) < 1):
             raise KeyError
 
         # Check for an equivalent UDP service
         i = 0
         hasEquivalent = False
-        while(i < (len(port2name[port]) - 1)):
+        while (i < (len(port2name[port]) - 1)):
             servicea = port2name[port][i]
             serviceb = port2name[port][i + 1]
-            if((servicea[2] == "tcp" and serviceb[2] == "udp") or (servicea[2] == "udp" and serviceb[2] == "tcp")):
+            if ((servicea[2] == "tcp" and serviceb[2] == "udp") or (servicea[2] == "udp" and serviceb[2] == "tcp")):
                 hasEquivalent = True
             i = i + 1
 
         # guaranteed to succeed, as we already raised KeyError on len(tcpservices) < 1
         for service in port2name[port]:
-            if(service[2] == "tcp"):
+            if (service[2] == "tcp"):
                 description = service[1]
                 break
 
-        if(hasEquivalent):
+        if (hasEquivalent):
             flags = 0x1
         else:
             flags = 0x0
@@ -170,19 +162,19 @@ while (port < pow(2,16)):
         # Skip over duplicate entries
         i = 0
         badService = False
-        while(i < (len(udpservices) - 1)):
-            if(udpservices[i][1] != udpservices[i + 1][1]):
+        while (i < (len(udpservices) - 1)):
+            if (udpservices[i][1] != udpservices[i + 1][1]):
                 print ("Service mismatch on port %u between:\n\t" % port) + udpservices[i][1] + "\n\tand:\n\t" + udpservices[i + 1][1] + "\n"
                 badService = True
             i = i + 1
 
-        if(badService or len(udpservices) < 1):
+        if (badService or len(udpservices) < 1):
             raise KeyError
 
         # Check for an equivalent TCP service
         i = 0
         hasEquivalent = False
-        while(i < (len(port2name[port]) - 1)):
+        while (i < (len(port2name[port]) - 1)):
             servicea = port2name[port][i]
             serviceb = port2name[port][i + 1]
             if((servicea[2] == "udp" and serviceb[2] == "tcp") or (servicea[2] == "tcp" and serviceb[2] == "udp")):
@@ -191,11 +183,11 @@ while (port < pow(2,16)):
 
         # guaranteed to succeed, as we already raised KeyError on len(udpservices) < 1
         for service in port2name[port]:
-            if(service[2] == "udp"):
+            if (service[2] == "udp"):
                 description = service[1]
                 break
 
-        if(hasEquivalent):
+        if (hasEquivalent):
             flags = 0x1
         else:
             flags = 0x0
