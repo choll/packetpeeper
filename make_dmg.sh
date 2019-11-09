471c34cb58c5f7048812be0ea2bf6721351775de
chris <chris@thor.local>
2019-11-09 02:38:57 +0000

    Remove fetch_and_build script, replace with git submodule and xcode sub-project

--- make_dmg.sh
+++ make_dmg.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -e
-./fetch_and_build_dependencies.sh
 DATE=$(date '+%Y-%m-%d')
-sed -i '.make_dmg_backup'  s/BUNDLE_VERSION_PLACEHOLDER/$DATE/g PacketPeeper.plist
+sed -i '.make_dmg_backup' s/BUNDLE_VERSION_PLACEHOLDER/$DATE/g PacketPeeper.plist
 xcodebuild -target All -configuration Release
 mv PacketPeeper.plist.make_dmg_backup PacketPeeper.plist
 hdiutil create -format UDBZ build/Release/PacketPeeper_$DATE.dmg -volname "Packet Peeper" -srcfolder 'build/Release/Packet Peeper.app'
